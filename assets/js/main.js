cf25bd3725087fea3c00c880147de5045004e39b
Michael Maschinoff <maschinov@zeppit.pro>
2018-10-29 14:09:37 +0200

    Examples from lecture #2

--- assets/js/main.js
+++ assets/js/main.js
@@ -149,6 +149,224 @@
  * Lecture #1 Friday
  */
 
+/**
+ * cough-0.js
+ *
+ * Michael Maschinoff
+ * maschinov@gmail.com
+ *
+ * Coughs three times.
+ *
+ * Demonstrates suboptimal design (and coughing).
+ */
+// document.writeln('cough');
+// document.writeln('cough');
+// document.writeln('cough');
+
+/**
+ * cough-1.js
+ *
+ * Michael Maschinoff
+ * maschinov@gmail.com
+ *
+ * Coughs three times.
+ *
+ * Demonstrates better design via a loop.
+ */
+// //cough three times
+// for(let i = 0; i < 3; i++){
+//     document.writeln('cough');
+// }
+
+/**
+ * cough-2.js
+ *
+ * Michael Maschinoff
+ * maschinov@gmail.com
+ *
+ * Coughs three times.
+ *
+ * Demonstrates abstraction and hierarchical decomposition.
+ */
+// for(let i = 0; i < 3; i++){
+//     cough();
+// }
+//
+// function cough(){
+//     document.writeln('cough');
+// }
+
+/**
+ * cough-3.js
+ *
+ * Michael Maschinoff
+ * maschinov@gmail.com
+ *
+ * Coughs three times.
+ *
+ * Demonstrates parameterization.
+ *
+ */
+// cough(3);
+//
+// function cough(n){
+//     for(let i = 0; i < n; i++) {
+//         document.writeln('cough');
+//     }
+// }
+
+/**
+ * cough-4.js
+ *
+ * Michael Maschinoff
+ * maschinov@gmail.com
+ *
+ * Coughs three times and sneezes three times.
+ *
+ * Demonstrates further abstraction.
+ */
+// cough(3);
+// sneeze(3);
+
+/**
+ * Cough n times
+ */
+// function cough(n){
+//     say('cough', n);
+// }
+
+/**
+ * Say word n times
+ */
+// function say(word, n){
+//     for(let i = 0; i < n; i++){
+//         document.writeln(word);
+//     }
+// }
+
+/**
+ * Sneeze n times
+ *
+ */
+// function sneeze(n){
+//     say('achoo')
+// }
+
+/**
+ * f2c.js
+ *
+ * Michael Maschinoff
+ * maschinov@gmail.com
+ *
+ * Converts Fahrenheit to Celsius.
+ *
+ * Demonstrates arithmetic.
+ */
+// // ask user for temperature in Fahrenheit
+// let f = prompt('Temperature in F: ');
+// // convert F to C
+// let c = 5.0 / 9.0 * (f - 32.0)
+//
+// // display result to one decimal place
+// document.writeln(parseFloat(c).toFixed(2));
+
+/**
+ * function-0.js
+ *
+ * Michael Maschinoff
+ * maschinov@gmail.com
+ *
+ * Print user's name.
+ *
+ * Demonstrates a function (not from a library) with a side effect.
+ */
+// let s = prompt('Your name: ');
+// printName(s);
+// // say hello to someone by name
+// function printName(name){
+//     document.writeln('hello, '+ name +'!');
+// }
+
+/**
+ * function-1.js
+ *
+ * Michael Maschinoff
+ * maschinov@gmail.com
+ *
+ * Demands that user provide a positive integer.
+ *
+ * Demonstrates use of a function (not from library) with a return value.
+ */
+// let n = getPositiveInt();
+// document.writeln('Thanks for the '+ n + '!');
+//
+// function getPositiveInt(){
+//     let n;
+//     do {
+//         n = +prompt('Please give me a positive int: ');
+//     }
+//     while (!n || n < 1);
+//     return n;
+// }
+
+/**
+ * return.js
+ *
+ * Michael Maschinoff
+ * maschinov@gmail.com
+ *
+ * Cubes a variable.
+ *
+ * Demonstrates use of parameter and return value.
+ */
+// let x = 2;
+// document.writeln('x is now '+ x);
+// document.writeln('Cubing...');
+// x = cube(x);
+// document.writeln('Cubed!');
+// document.writeln('x is now '+ x);
+//
+// // Cube argument
+// function cube(n){
+//     return n * n * n;
+// }
+
+/**
+ * switch.js
+ *
+ * Michael Maschinoff
+ * maschinov@gmail.com
+ *
+ * Assesses the size of user's input.
+ *
+ * Demonstrate use of a switch.
+ */
+
+// // ask user for an integer
+// let n = +prompt('Give me an integer between 1 and 10: ');
+//
+// // judge user's input
+// switch(n){
+//     case 1:
+//     case 2:
+//     case 3:
+//         document.writeln('You picked a small number.');
+//         break;
+//     case 4:
+//     case 5:
+//     case 6:
+//         document.writeln('You picked a medium number.');
+//         break;
+//     case 7:
+//     case 8:
+//     case 9:
+//     case 10:
+//         document.writeln('You picked a big number.');
+//         break;
+//     default:
+//         document.writeln('Your picked an invalid number.');
+//         break;
+// }
 
 // while(true)
 // {
@@ -327,3 +545,4 @@
 //         }
 //     }
 // }
+
